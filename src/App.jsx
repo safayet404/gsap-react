d38d0ea831a52851e6741be9274ef9368d936e9a
Safayet <hossainsafayet187@gmail.com>
2025-11-27 02:29:52 +0600

    nav menu

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import gsap from "gsap";
 import { ScrollTrigger, SplitText } from "gsap/all";
+import Navbar from "./components/Navbar";
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 const App = () => {
-  return <div className="text-red-600 text-3xl flex-center">App</div>;
+  return (
+    <main>
+      <Navbar />
+    </main>
+  );
 };
 
 export default App;
---
+++ src/constants/index.js
@@ -0,0 +1,4 @@
+export const navLinks = [
+  { id: "cocktail", title: "Cocktails" },
+  { id: "about", title: "About" },
+];
