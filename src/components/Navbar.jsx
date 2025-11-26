e2b40336065d2a5f86114b627ae0ddcdbc924704
Safayet <hossainsafayet187@gmail.com>
2025-11-27 03:13:30 +0600

    title and subtitle animated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,32 @@
 import React from "react";
 import { navLinks } from "../constants";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
 
 const Navbar = () => {
+  useGSAP(() => {
+    const navTween = gsap.timeline({
+      scrollTrigger: {
+        trigger: "nav",
+        start: "bottom top",
+      },
+    });
+    navTween.fromTo(
+      "nav",
+      { backgroundColor: "transparent" },
+      {
+        backgroundColor: "#00000050",
+        backgroundFilter: "blur(10px)",
+        duration: 1,
+        ease: "power1.inOut",
+      }
+    );
+  });
   return (
     <nav>
       <div>
         <a href="#home" className="flex items-center gap-2">
+          <img alt="logo" src="/images/logo.png" />
           Velvet Pour
         </a>
 
