fcb337b9a960ad8493a2944659105e0fa215e1a6
Safayet <hossainsafayet187@gmail.com>
2025-11-27 21:40:06 +0600

    cocktail leaf animation

--- src/components/Cocktails.jsx
+++ src/components/Cocktails.jsx
@@ -1,7 +1,29 @@
-import React from "react";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
 import { cocktailLists, mockTailLists } from "../constants";
 
 const Cocktails = () => {
+  useGSAP(() => {
+    const parallaxTimeline = gsap.timeline({
+      scrollTrigger: {
+        trigger: "#cocktails",
+        start: "top 30%",
+        end: "bottom 80%",
+        scrub: true,
+      },
+    });
+
+    parallaxTimeline
+      .from("#c-left-leaf", {
+        x: -100,
+        y: 100,
+      })
+      .from("#c-right-leaf", {
+        x: 100,
+        y: 100,
+      });
+  });
+
   return (
     <section id="cocktails" className="noisy">
       <img src="/images/cocktail-left-leaf.png" alt="l-leaf" id="c-left-leaf" />
@@ -13,36 +35,36 @@ const Cocktails = () => {
 
       <div className="list">
         <div className="popular">
-          <h2>Most Popular cocktails</h2>
+          <h2>Most popular cocktails:</h2>
 
           <ul>
-            {cocktailLists.map((drink) => (
-              <li key={drink.name}>
+            {cocktailLists.map(({ name, country, detail, price }) => (
+              <li key={name}>
                 <div className="md:me-28">
-                  <h3>{drink.name}</h3>
-                  <h3>
-                    {drink.country} | {drink.detail}{" "}
-                  </h3>
+                  <h3>{name}</h3>
+                  <p>
+                    {country} | {detail}
+                  </p>
                 </div>
-                <span> - {drink.price} </span>
+                <span>- {price}</span>
               </li>
             ))}
           </ul>
         </div>
 
         <div className="loved">
-          <h2>Most loved mocktails</h2>
+          <h2>Most loved mocktails:</h2>
 
           <ul>
-            {mockTailLists.map((drink) => (
-              <li key={drink.name}>
-                <div className="md:me-28">
-                  <h3>{drink.name}</h3>
-                  <h3>
-                    {drink.country} | {drink.detail}{" "}
-                  </h3>
+            {mockTailLists.map(({ name, country, detail, price }) => (
+              <li key={name}>
+                <div className="me-28">
+                  <h3>{name}</h3>
+                  <p>
+                    {country} | {detail}
+                  </p>
                 </div>
-                <span> - {drink.price} </span>
+                <span>- {price}</span>
               </li>
             ))}
           </ul>
